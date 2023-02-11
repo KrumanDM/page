7604f3ee23b99091c0a0e2b68ea26479ed6589ec
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-02-11 13:06:19 +0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,42 @@ menuClose.addEventListener( 'click', ( event ) => {
 // Валидация формы
 
 function validate (event){
-    console.log("функция выполнена");
+
+	const errorContainer = document.getElementById( 'errors' );
+    // Очищаем все ошибки перед новой валидацией формы
+	errorContainer.innerHTML = '';
+    
+
+	let valid = true;
+    
+	//Проверка имени
+	const nameField = document.getElementById( 'name' );
+	if ( ! nameField.value || nameField.value.length < 3 ){
+      errorContainer.innerHTML += '<p style="color:red"> Поле имени ни разу не ок</p>';
+	valid = false;
+	}
+
+	//Проверка email
+	const emailField = document.getElementById( 'email' ),
+          re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+	if ( ! emailField.value || ! re.test(String(emailField.value).toLowerCase()) ){
+      errorContainer.innerHTML += '<p style="color:red"> Ваш email не заполнен или не корректен</p>';
+	valid = false;
+	}
+
+	//Проверка сообщений
+	const msgField = document.getElementById( 'message' );
+	if ( ! msgField.value || msgField.value.length < 10 ){
+      errorContainer.innerHTML += '<p style="color:red"> Поле сообщения не заполнено</p>';
+	valid = false;
+	}
+
+	// Если была хотя бы одна ошибка
+	if ( false === valid ) {
+	event.preventDefault();
+	}
+	return valid;
 }
 
 
